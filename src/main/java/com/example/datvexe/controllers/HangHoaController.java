08fa32c27b407893331fd0dbc067b99ef2c0d696
VoDuy89123 <votruongduy1943@gmail.com>
2023-02-22 21:55:08 +0700

    finish project

--- src/main/java/com/example/datvexe/controllers/HangHoaController.java
+++ src/main/java/com/example/datvexe/controllers/HangHoaController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "https://duyvotruong.github.io/Dat-ve-xe-front-end")
+@CrossOrigin(origins = "https://duyvotruong.github.io/Dat-ve-xe-front-end/#")
 @RequestMapping("/api/hanghoa")
 public class HangHoaController {
 
